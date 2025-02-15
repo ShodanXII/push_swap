ac5cc684b0daec9d82a41e5254cd64060ef8112b
Abde Nnour Chat <achat@c2r1p6.1337.ma>
2025-02-15 12:56:58 +0100

    makefile

--- push_wap/push_swap.h
+++ push_wap/push_swap.h
@@ -10,22 +10,22 @@
 
 typedef struct s_node
 {
-	int             value;
-	int            index;
-	struct s_node   *next;
+	int				value;
+	int				index;
+	struct s_node	*next;
 } t_node;
 
 typedef struct s_stack
 {
-	t_node  *top;
-	int     size;
+	t_node	*top;
+	int		size;
 } t_stack;
 
 t_stack *create_stack(void);
-void    push_to_stack(t_stack *stack_a, t_stack *stack_b);
-int     is_empty(t_stack *stack);
-void    free_stack(t_stack *stack);
-int 	error(void);
+void	push_to_stack(t_stack *stack_a, t_stack *stack_b);
+int		is_empty(t_stack *stack);
+void	free_stack(t_stack *stack);
+int		error(void);
 void	data_init(char **av);
 void	sort_stack(t_stack *stack);
 void	index_stack(t_stack *stack);
